2c476ff6415f154fc920d8f6900819f0e455214f
Quentin <fle.quentin@gmail.com>
2023-02-02 16:44:19 +0100

    Implement CalixInputComponent

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,12 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'angularWithCalixUi';
+  value: string | null = '';
+
+  changeValue(event: string | null) {
+    this.value = event;
+  }
 }
